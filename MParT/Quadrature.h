dcc7164dbc16435034d32f52b9faa0010f1e15e6
Ricardo Baptista <rsb@mit.edu>
2022-01-22 10:20:49 -0500

    adding recursive quadrature

--- MParT/Quadrature.h
+++ MParT/Quadrature.h
@@ -80,6 +80,13 @@ public:
         return std::make_pair(wts,pts);
     }
     
+    /**
+     @brief Approximates the integral \f$\int_{L}^{U} f(x) dx\f$ using a Clenshaw-Curtis quadrature rule.
+     @param[in] f The integrand
+     @param[in] lb The lower bound \f$L\f$ in the integration.
+     @param[in] lb The upper bound \f$U\f$ in the integration.
+     @returns A pair containing (wts,pts)
+     */
     template<class ScalarFuncType>
     double Integrate(ScalarFuncType const& f, 
                     double                 lb, 
@@ -118,7 +125,7 @@ public:
        @param[in] absTol An absolute error tolerance used to stop the adaptive integration.
        @param[in] relTol A relative error tolerance used to stop te adaptive integration.
      */
-    RecursiveQuadrature(unsigned int maxSub, double absTol=1e-8, double relTol=1e-10) : _maxSub(maxSub), _absTol(absTol), _relTol(relTol)
+    RecursiveQuadrature(unsigned int maxSub, unsigned int order, double absTol=1e-8, double relTol=1e-10) : _maxSub(maxSub), _order(order), _absTol(absTol), _relTol(relTol), quad(order)
     {
         if(absTol<=0){
             std::stringstream msg;
@@ -136,6 +143,7 @@ public:
             msg << "In MParT::GaussKronrad: Maximum subintervals allowed must be greater than 0, but given a value of \"" << maxSub << "\".";
             throw std::runtime_error(msg.str());
         }
+        //ClenshawCurtisQuadrature quad(_order);
 
     }
 
@@ -156,12 +164,62 @@ public:
         //integral = quad.Integrate(f,lb,ub);
         integral = 1.0;
         return integral;
+        
+    }
+
+    template<class ScalarFuncType>
+    double RecursiveIntegrate(ScalarFuncType const& f, 
+                            double lb, 
+                            double ub, 
+                            int level, 
+                            double I1) {
+
+        // update current refinement level
+        level = level + 1;
+
+        // evaluate I1 on first call of function
+        if (level == 1) {
+            double I1;
+            I1 = quad.Integrate(f, lb, ub);
+        }
+
+        // evluate integral on each sub-interval
+        double mb, I2_left, I2_right;
+        mb = lb+0.5*(ub-lb);
+        I2_left  = quad.Integrate(f, lb, mb);
+        I2_right = quad.Integrate(f, mb, ub);
+        // compute total integral
+        double I2;
+        I2 = I2_left + I2_right;
+
+        // Stop the recursion if the recursion has hit maximum depth
+        if (level == _maxSub) {
+            return I2;
+        }
+
+        // If the difference between levels is lower than tolerance, stop the
+        // recursion and accept the fine level result, else subdivide further
+        double I;
+        if ( std::abs(I2-I1) < std::fmax( _relTol*std::abs(I1), _absTol) ) {
+            I = I2;
+        } else {
+            // apply recursion in sub-domains
+            double I_left, I_right;
+            I_left = RecursiveIntegrate(f, lb, mb, level, I2_left);
+            I_right = RecursiveIntegrate(f, mb, ub, level, I2_right);
+            // compute total integral
+            I = I_left + I_right;
+        }
+        return I;
+
     }
 
 private:
     unsigned int _maxSub;
+    unsigned int _order;
     double _absTol;
     double _relTol;
+    ClenshawCurtisQuadrature quad;
 
 }; // class RecursiveQuadrature
 
