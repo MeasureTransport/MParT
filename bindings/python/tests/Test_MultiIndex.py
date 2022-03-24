257734be79f2b7255d0a9a9419b4e28ab22e43a6
Michael C Brennan <mcbrenn@mit.edu>
2022-03-24 14:56:58 -0400

    Adding some basic unit tests for python MultiIndex

--- bindings/python/tests/Test_MultiIndex.py
+++ bindings/python/tests/Test_MultiIndex.py
@@ -1,11 +1,30 @@
 # MultiIndex test
 
 import mpart
+import unittest
 
-def test_always_passes():
-    assert True
+class TestStringMethods(unittest.TestCase):
 
-def test_always_fails():
-    assert False
+    def test_sum(self):
+        
+        idx = mpart.MultiIndex(30,2)
+
+        
+        assert idx.sum() == 60
+
+    def test_max(self):
+
+        idx = mpart.MultiIndex(30,2)
+        idx[0] = 100
+
+        assert idx.max() == 100
+
+    def test_count_nonzero(self):
+        idx = mpart.MultiIndex(30,0)
+
+        idx[3] = 1
+        idx[26] = 3
+        assert idx.count_nonzero() == 2
+
 
 
