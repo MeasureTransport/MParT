be8b3ae633e5121c52537782aeacb56dc0fbed90
Danny Sharp <dannys4@mit.edu>
2022-04-29 10:27:58 -0400

    Fixing how to wrap Julia base fcns

--- bindings/julia/src/Wrapper.cpp
+++ bindings/julia/src/Wrapper.cpp
@@ -10,10 +10,10 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
         .constructor<unsigned int, unsigned int>()
         .constructor<std::vector<unsigned int> const&>()
         .method("count_nonzero", &MultiIndex::NumNz);
-    // mod.set_override_module(jl_base_module);
-    mod.method("sum", &MultiIndex::Sum)
-       .method("max", &MultiIndex::Max)
-       .method("setindex", &MultiIndex::Set);
+    mod.set_override_module(jl_base_module);
+    mod.method("sum", [](MultiIndex idx){ return idx.Sum(); });
+    //    .method("max", &MultiIndex::Max)
+    //    .method("setindex", &MultiIndex::Set)
     //    .method("getindex", &MultiIndex::Get)
     //    .method("String", &MultiIndex::String)
     //    .method("length", &MultiIndex::Length)
@@ -23,5 +23,5 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
     //    .method(">", &MultiIndex::operator>)
     //    .method("<=", &MultiIndex::operator<=)
     //    .method(">=", &MultiIndex::operator>=);
-    // mod.unset_override_module();
+    mod.unset_override_module();
 }
