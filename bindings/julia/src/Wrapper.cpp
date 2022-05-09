80f152e6c6b56c811fc6536b429d7e76771495c0
Matthew Parno <parnomd@gmail.com>
2022-05-09 10:37:19 -0400

    Fixed a few bugs in Julia wrapper related to pass by reference.

--- bindings/julia/src/Wrapper.cpp
+++ bindings/julia/src/Wrapper.cpp
@@ -1,6 +1,8 @@
 #include "CommonJuliaUtilities.h"
 #include "MParT/MultiIndices/MultiIndex.h"
 
+#include <iostream>
+
 using namespace mpart;
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
@@ -12,17 +14,18 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
         .constructor<std::vector<unsigned int> const&>()
         .method("count_nonzero", &MultiIndex::NumNz);
     mod.set_override_module(jl_base_module);
-    mod.method("sum", [](MultiIndex idx){ return idx.Sum(); });
-    mod.method("setindex!", [](MultiIndex idx, unsigned int val, unsigned int ind) { return idx.Set(ind, val); });
-    mod.method("getindex", [](MultiIndex idx, unsigned int ind) { return idx.Get(ind); });
-    mod.method("maximum", [](MultiIndex idx){ return idx.Max(); });
-    mod.method("String", [](MultiIndex idx){ return idx.String().c_str(); });
-    mod.method("length", [](MultiIndex idx){ return idx.Length(); });
-    mod.method("==", [](MultiIndex idx1, MultiIndex idx2){ return idx1 == idx2; });
-    mod.method("!=", [](MultiIndex idx1, MultiIndex idx2){ return idx1 != idx2; });
-    mod.method("<", [](MultiIndex idx1, MultiIndex idx2){ return idx1 < idx2; });
-    mod.method(">", [](MultiIndex idx1, MultiIndex idx2){ return idx1 > idx2; });
-    mod.method("<=", [](MultiIndex idx1, MultiIndex idx2){ return idx1 <= idx2; });
-    mod.method(">=", [](MultiIndex idx1, MultiIndex idx2){ return idx1 >= idx2; });
+    mod.method("sum", [](MultiIndex const& idx){ return idx.Sum(); });
+    mod.method("setindex!", [](MultiIndex& idx, unsigned int val, unsigned int ind) { return idx.Set(ind, val); });
+    mod.method("getindex", [](MultiIndex const& idx, unsigned int ind) { return idx.Get(ind); });
+    mod.method("maximum", [](MultiIndex const& idx){ return idx.Max(); });
+    mod.method("String", [](MultiIndex const& idx){ return idx.String(); });
+    mod.method("length", [](MultiIndex const& idx){ return idx.Length(); });
+    mod.method("==", [](MultiIndex const& idx1, MultiIndex const& idx2){ return idx1 == idx2; });
+    mod.method("!=", [](MultiIndex const& idx1, MultiIndex const& idx2){ return idx1 != idx2; });
+    mod.method("<", [](MultiIndex const& idx1, MultiIndex const& idx2){ return idx1 < idx2; });
+    mod.method(">", [](MultiIndex const& idx1, MultiIndex const& idx2){ return idx1 > idx2; });
+    mod.method("<=", [](MultiIndex const& idx1, MultiIndex const& idx2){ return idx1 <= idx2; });
+    mod.method(">=", [](MultiIndex const& idx1, MultiIndex const& idx2){ return idx1 >= idx2; });
+    mod.method("print", [](MultiIndex const& idx){std::cout << idx.String() << std::flush;});
     mod.unset_override_module();
 }
