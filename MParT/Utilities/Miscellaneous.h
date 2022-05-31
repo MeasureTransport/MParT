37668458a4f5dff8c261a58983c281bbb24c9599
dannys4 <dannys4@vt.edu>
2022-05-31 11:03:43 -0400

    Change swap to use `std::move`

--- MParT/Utilities/Miscellaneous.h
+++ MParT/Utilities/Miscellaneous.h
@@ -7,17 +7,17 @@
 
 namespace mpart{
 
-    template<typename T> 
+    template<typename T>
     KOKKOS_INLINE_FUNCTION void swap(T& t1, T& t2) {
-        T temp = t1;
-        t1 = t2;
-        t2 = temp;
+        T temp = std::move(t1);
+        t1 = std::move(t2);
+        t2 = std::move(temp);
     }
 
     /** Tries to read an options from a std::map.  If the key does not exist, the specified default value is returned. */
-    std::string GetOption(std::unordered_map<std::string,std::string> const& map, 
-                          std::string                                 const& key, 
+    std::string GetOption(std::unordered_map<std::string,std::string> const& map,
+                          std::string                                 const& key,
                           std::string                                 const& defaultValue);
 }
 
-#endif 
+#endif
