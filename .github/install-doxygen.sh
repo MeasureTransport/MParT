e6c40ec986a1be1ed8cf73494a42088e370fdfb5
dannys4 <dannys4@vt.edu>
2022-06-10 12:22:02 -0400

    remove pushd

--- .github/install-doxygen.sh
+++ .github/install-doxygen.sh
@@ -6,8 +6,8 @@
 mkdir tmp-docs && pushd tmp-docs
 wget https://www.doxygen.nl/files/doxygen-$1.linux.bin.tar.gz
 tar -xzf doxygen-$1.linux.bin.tar.gz
-pushd doxygen-$1
+cd doxygen-$1
 sudo make install
-popd +2
+cd ../..
 sudo apt install libclang1-9
 sudo apt install libclang-cpp9
