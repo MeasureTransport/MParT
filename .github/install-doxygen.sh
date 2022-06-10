5a295811e820d55151bfee8756b72fd53fdb66e3
dannys4 <dannys4@vt.edu>
2022-06-10 12:23:32 -0400

    remove pushd even more

--- .github/install-doxygen.sh
+++ .github/install-doxygen.sh
@@ -3,7 +3,7 @@
 # install doxygen
 # $1 is the version of Doxygen
 
-mkdir tmp-docs && pushd tmp-docs
+mkdir tmp-docs && cd tmp-docs
 wget https://www.doxygen.nl/files/doxygen-$1.linux.bin.tar.gz
 tar -xzf doxygen-$1.linux.bin.tar.gz
 cd doxygen-$1
