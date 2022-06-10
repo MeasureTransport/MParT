18bc3582e90857480840821685dca377bad8cda5
dannys4 <dannys4@vt.edu>
2022-06-10 12:20:38 -0400

    Remove filename check

--- .github/install-doxygen.sh
+++ .github/install-doxygen.sh
@@ -3,14 +3,11 @@
 # install doxygen
 # $1 is the version of Doxygen
 
-if [ ! -f $1 ]; then # Credit to mattnotmitt/doxygen-action
-  echo "File $1 could not be found!"
-  exit 1
-fi
-
 mkdir tmp-docs && pushd tmp-docs
 wget https://www.doxygen.nl/files/doxygen-$1.linux.bin.tar.gz
 tar -xzf doxygen-$1.linux.bin.tar.gz
 pushd doxygen-$1
 sudo make install
 popd +2
+sudo apt install libclang1-9
+sudo apt install libclang-cpp9
