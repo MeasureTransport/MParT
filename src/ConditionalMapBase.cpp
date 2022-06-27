8bd7c61498526144f39e37610dd6cddc27500f5a
dannys4 <dannys4@vt.edu>
2022-06-27 07:36:39 -0400

    Start Templating ConditionalMapBase

--- src/ConditionalMapBase.cpp
+++ src/ConditionalMapBase.cpp
@@ -3,30 +3,32 @@
 
 using namespace mpart;
 
-Kokkos::View<double**, Kokkos::HostSpace> ConditionalMapBase::Evaluate(Kokkos::View<const double**, Kokkos::HostSpace> const& pts)
+template<typename MemorySpace>
+Kokkos::View<double**, MemorySpace> ConditionalMapBase<MemorySpace>::Evaluate(Kokkos::View<const double**, MemorySpace> const& pts)
 {
-    Kokkos::View<double**, Kokkos::HostSpace> output("Map Evaluations", outputDim, pts.extent(1));
+    Kokkos::View<double**, MemorySpace> output("Map Evaluations", outputDim, pts.extent(1));
     EvaluateImpl(pts, output);
     return output;
 }
 
-Eigen::RowMatrixXd ConditionalMapBase::Evaluate(Eigen::RowMatrixXd const& pts)
+template<typename MemorySpace>
+Eigen::RowMatrixXd ConditionalMapBase<MemorySpace>::Evaluate(Eigen::RowMatrixXd const& pts)
 {
     Eigen::RowMatrixXd output(outputDim, pts.cols());
-    Kokkos::View<const double**, Kokkos::HostSpace> ptsView = ConstRowMatToKokkos<double>(pts);
-    Kokkos::View<double**, Kokkos::HostSpace> outView = MatToKokkos<double>(output);
+    Kokkos::View<const double**, MemorySpace> ptsView = ConstRowMatToKokkos<double>(pts);
+    Kokkos::View<double**, MemorySpace> outView = MatToKokkos<double>(output);
     EvaluateImpl(ptsView, outView);
     return output;
 }
 
-
-void ConditionalMapBase::SetCoeffs(Kokkos::View<double*, Kokkos::HostSpace> coeffs){
+template<typename MemorySpace>
+void ConditionalMapBase<MemorySpace>::SetCoeffs(Kokkos::View<double*, MemorySpace> coeffs){
 
     // If coefficients already exist, make sure the sizes match
     if(this->savedCoeffs.is_allocated()){
         if(coeffs.size() != numCoeffs){
             std::stringstream msg;
-            msg << "Error in ConditionalMapBase::SetCoeffs.  Expected coefficient vector with size " << numCoeffs << ", but new coefficients have size " << coeffs.size() << ".";
+            msg << "Error in ConditionalMapBase<MemorySpace>::SetCoeffs.  Expected coefficient vector with size " << numCoeffs << ", but new coefficients have size " << coeffs.size() << ".";
             throw std::invalid_argument(msg.str());
         }
 
@@ -34,32 +36,33 @@ void ConditionalMapBase::SetCoeffs(Kokkos::View<double*, Kokkos::HostSpace> coef
             Kokkos::resize(this->savedCoeffs, numCoeffs);
     }else{
 
-        this->savedCoeffs = Kokkos::View<double*, Kokkos::HostSpace>("ConditionalMapBase Coefficients", coeffs.size());
+        this->savedCoeffs = Kokkos::View<double*, MemorySpace>("ConditionalMapBase<MemorySpace> Coefficients", coeffs.size());
     }
 
     Kokkos::deep_copy(this->savedCoeffs, coeffs);
 }
 
-Kokkos::View<double*, Kokkos::HostSpace> ConditionalMapBase::LogDeterminant(Kokkos::View<const double**, Kokkos::HostSpace> const& pts)
+template<typename MemorySpace>
+Kokkos::View<double*, MemorySpace> ConditionalMapBase<MemorySpace>::LogDeterminant(Kokkos::View<const double**, MemorySpace> const& pts)
 {
-    Kokkos::View<double*, Kokkos::HostSpace> output("Log Determinants", pts.extent(1));
+    Kokkos::View<double*, MemorySpace> output("Log Determinants", pts.extent(1));
     LogDeterminantImpl(pts, output);
     return output;
 }
 
-Eigen::VectorXd ConditionalMapBase::LogDeterminant(Eigen::RowMatrixXd const& pts)
+template<typename MemorySpace>
+Eigen::VectorXd ConditionalMapBase<MemorySpace>::LogDeterminant(Eigen::RowMatrixXd const& pts)
 {
     Eigen::VectorXd output(pts.cols());
-    Kokkos::View<const double**, Kokkos::HostSpace> ptsView = ConstRowMatToKokkos<double>(pts);
-    Kokkos::View<double*, Kokkos::HostSpace> outView = VecToKokkos<double>(output);
+    Kokkos::View<const double**, MemorySpace> ptsView = ConstRowMatToKokkos<double>(pts);
+    Kokkos::View<double*, MemorySpace> outView = VecToKokkos<double>(output);
     LogDeterminantImpl(ptsView, outView);
     return output;
 }
 
-
-
-Kokkos::View<double**, Kokkos::HostSpace> ConditionalMapBase::Inverse(Kokkos::View<const double**, Kokkos::HostSpace> const& x1,
-                                                                      Kokkos::View<const double**, Kokkos::HostSpace> const& r)
+template<typename MemorySpace>
+Kokkos::View<double**, MemorySpace> ConditionalMapBase<MemorySpace>::Inverse(Kokkos::View<const double**, MemorySpace> const& x1,
+                                                                      Kokkos::View<const double**, MemorySpace> const& r)
 {
     // Throw an error if the inputs don't have the same number of columns
     if(x1.extent(1)!=r.extent(1)){
@@ -68,31 +71,39 @@ Kokkos::View<double**, Kokkos::HostSpace> ConditionalMapBase::Inverse(Kokkos::Vi
         throw std::invalid_argument(msg.str());
     }
 
-    Kokkos::View<double**, Kokkos::HostSpace> output("Map Inverse Evaluations", outputDim, r.extent(1));
+    Kokkos::View<double**, MemorySpace> output("Map Inverse Evaluations", outputDim, r.extent(1));
     InverseImpl(x1,r, output);
     return output;
 }
 
-
-Eigen::RowMatrixXd ConditionalMapBase::Inverse(Eigen::RowMatrixXd const& x1, Eigen::RowMatrixXd const& r)
+template<typename MemorySpace>
+Eigen::RowMatrixXd ConditionalMapBase<MemorySpace>::Inverse(Eigen::RowMatrixXd const& x1, Eigen::RowMatrixXd const& r)
 {
     Eigen::RowMatrixXd output(inputDim, r.cols());
 
-    Kokkos::View<const double**, Kokkos::HostSpace> x1View = ConstRowMatToKokkos<double>(x1);
-    Kokkos::View<const double**, Kokkos::HostSpace> rView = ConstRowMatToKokkos<double>(r);
-    Kokkos::View<double**, Kokkos::HostSpace> outView = MatToKokkos<double>(output);
+    Kokkos::View<const double**, MemorySpace> x1View = ConstRowMatToKokkos<double>(x1);
+    Kokkos::View<const double**, MemorySpace> rView = ConstRowMatToKokkos<double>(r);
+    Kokkos::View<double**, MemorySpace> outView = MatToKokkos<double>(output);
 
     InverseImpl(x1View, rView, outView);
     return output;
 }
 
-
-Eigen::Map<Eigen::VectorXd> ConditionalMapBase::CoeffMap()
+template<typename MemorySpace>
+Eigen::Map<Eigen::VectorXd> ConditionalMapBase<MemorySpace>::CoeffMap()
 {
     return KokkosToVec(this->savedCoeffs);
 }
 
-// Eigen::Map<const Eigen::VectorXd> ConditionalMapBase::CoeffMap() const
+// Eigen::Map<const Eigen::VectorXd> ConditionalMapBase<MemorySpace>::CoeffMap() const
 // {
 //     return KokkosToVec(this->savedCoeffs);
 // }
+
+// Explicit template instantiation
+#if defined(KOKKOS_ENABLE_CUDA ) || defined(KOKKOS_ENABLE_SYCL)
+    template class mpart::ConditionalMapBase<Kokkos::HostSpace>;
+    template class mpart::ConditionalMapBase<Kokkos::DefaultExecutionSpace::memory_space>;
+#else
+    template class mpart::ConditionalMapBase<Kokkos::HostSpace>;
+#endif
