7a03a57e6a82dfafc5ed2c051cf8d7616f477ac3
Michael C Brennan <mcbrenn@mit.edu>
2022-06-28 11:05:51 -0400

    Small add to MultiIndexSet tests

--- bindings/python/tests/Test_MultiIndexSet.py
+++ bindings/python/tests/Test_MultiIndexSet.py
@@ -9,8 +9,16 @@ class TestStringMethods(unittest.TestCase):
     def test_max_degrees(self):
         multis = np.array([[0, 1], [2, 0]])
         mset = mpart.MultiIndexSet(multis)
-        
         assert np.all(mset.MaxOrders() == [2,1])
+        
+        noneLim = mpart.NoneLim()
+        mset = mpart.MultiIndexSet.CreateTensorProduct(3,4,noneLim)
+        assert np.all(mset.MaxOrders() == [4,4,4])
+
+        mset = mpart.MultiIndexSet.CreateTotal(3,4,noneLim)
+        assert np.all(mset.MaxOrders() == [4,4,4])
+
+
 
 
 
