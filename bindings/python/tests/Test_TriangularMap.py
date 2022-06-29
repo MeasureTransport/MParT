2be84521466c0982d08f1004a01888c5a84f9526
Michael C Brennan <mcbrenn@mit.edu>
2022-06-29 15:03:36 -0400

    TriangularMap tests done

--- bindings/python/tests/Test_TriangularMap.py
+++ bindings/python/tests/Test_TriangularMap.py
@@ -6,19 +6,8 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-    # def test_CreateComponent(self):
-    #     noneLim = mpart.NoneLim()
-    #     mset = mpart.MultiIndexSet.CreateTotalOrder(1,3, noneLim)
-    #     # Set MapOptions and make map
-    #     opts = mpart.MapOptions()
-    #     component = mpart.CreateComponent(mset.fix(True), opts)
-    #     assert component.numCoeffs == 4
-    #     assert np.all(component.CoeffMap() == [0.,0.,0.,0.])
-
-
-
-
-    def test_init(self):
+    def setUp(self):
+    
         opts = mpart.MapOptions()
 
         multis_1 = np.array([[0],[1]])  # linear
@@ -30,20 +19,39 @@ class TestStringMethods(unittest.TestCase):
         map_1 = mpart.CreateComponent(mset_1, opts)
         map_2 = mpart.CreateComponent(mset_2, opts)
 
-        triangular = mpart.TriangularMap([map_1,map_2])
+        self.triangular = mpart.TriangularMap([map_1,map_2])
+        self.num_samples = 100
+        self.x = np.random.randn(2,self.num_samples)
+
+
+    def test_numCoeffs(self):
+        assert self.triangular.numCoeffs == 2 + 3
+
+
+    def test_CoeffsMap(self):
+        assert self.triangular.CoeffMap().tolist() == []
+        self.triangular.SetCoeffs(np.zeros(self.triangular.numCoeffs))
+        assert self.triangular.CoeffMap().tolist() == [0,0,0,0,0]
+
+
+    def test_Evaluate(self):    
+        assert np.all(np.abs(self.triangular.Evaluate(self.x) - self.x) < 1E-6)
+
+
+    def test_LogDeterminant(self):
+        assert np.all(self.triangular.LogDeterminant(self.x) == np.zeros(self.num_samples))
+        self.triangular.SetCoeffs(np.array([0., 3.3, 0., 0., 0.]))
+        assert np.all(self.triangular.LogDeterminant(self.x) == 3.3*np.ones(self.num_samples))
+
+
+    def test_Inverse(self):    
+        self.triangular.SetCoeffs(np.array([0.,1.,0.,0.,0.]))
+        y = self.triangular.Evaluate(self.x)
+        x_ = self.triangular.Inverse(np.zeros((0,self.num_samples)),y)
+        assert np.all(np.abs(x_ - self.x) < 1E-6)
+
         
-        assert triangular.numCoeffs == 2 + 3
-        assert triangular.CoeffMap().tolist() == []
 
-        triangular.SetCoeffs(np.zeros(triangular.numCoeffs))
-        assert triangular.CoeffMap().tolist() == [0,0,0,0,0]
-
-        x = np.random.randn(2,100)
-        np.all(np.abs(triangular.Evaluate(x) - x) < 1E-12)
-
-        triangular.SetCoeffs(np.array([0.,1.,0.,0.,0.]))
-        y = triangular.Evaluate(x)
-        x_ = triangular.InverseInplace(y)
 
 
         
