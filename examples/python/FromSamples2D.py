a5fd195346e2fa076b16f16cc99fb610f76ab3ff
Michael C Brennan <mcbrenn@mit.edu>
2022-06-29 09:49:42 -0400

    Added Banana example

--- examples/python/FromSamples2D.py
+++ examples/python/FromSamples2D.py
@@ -1,7 +1,7 @@
 from mpart import *
 import numpy as np
 from scipy.optimize import minimize
-from scipy.stats import norm
+from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 
 
@@ -22,53 +22,68 @@ def sinharcsinh(z,loc,scale,skew,tail):
 
 # Make target samples
 num_points = 5000
-z = np.random.randn(num_points)
-x = sinharcsinh(z, loc=-1, scale=1, skew=.5, tail=1)
+z = np.random.randn(2,num_points)
+x1 = z[0]
+x2 = z[1] + z[0]**2
+x = np.vstack([x1,x2])
 # x = -2 + .5*z  # For Gaussian test case
 # For plotting and computing reference density 
-rv = norm()
+rv = multivariate_normal(np.zeros(2),np.eye(2))
 t = np.linspace(-3,3,100)
-rho_t = rv.pdf(t)
+T1, T2 = np.meshgrid(t,t)
+rho_t = rv.pdf(np.dstack((T1,T2)))
 
 # Before optimization
-num_bins = 50
+num_bins = 10
 plt.figure()
-plt.hist(x, num_bins, facecolor='blue', alpha=0.5, density=True, label='Target samples')
-plt.plot(t,rho_t,label="Reference density")
-plt.legend()
+#plt.hist2d(x[0],x[1], num_bins, facecolor='blue', alpha=0.5, density=True, label='Target samples')
+plt.contour(T1,T2,rho_t,label="Reference density")
+plt.scatter(x[0],x[1], facecolor='blue', alpha=0.1, label='Target samples')
+# plt.legend()
 plt.show()
 
-# Create multi-index set:
-multis = np.array([[0], [1], [2], [3], [4], [5]])
-# multis = np.array([[0], [1]])  # For Gaussian test case
-mset = MultiIndexSet(multis)
-fixed_mset = mset.fix(True)
 
 # Set MapOptions and make map
 opts = MapOptions()
-map = CreateComponent(fixed_mset, opts)
+multis_1 = np.array([[0],[1]])
+multis_2 = np.array([[0,0],[0,1],[2,0]])
+
+mset_1 = MultiIndexSet(multis_1).fix(True)
+mset_2 = MultiIndexSet(multis_2).fix(True)
+
+map_1 = CreateComponent(mset_1,opts)
+map_2 = CreateComponent(mset_2,opts)
+
+tri_map = TriangularMap([map_1,map_2])
+# tri_map = CreateTriangular(2,2,2,opts)
+coeffs = np.zeros(tri_map.numCoeffs)
+tri_map.SetCoeffs(coeffs)
+
 
 # KL divergence objective
 def objective(coeffs):
-    map.SetCoeffs(coeffs)
-    map_of_x = map.Evaluate(x.reshape(1,num_points))
-    rho_of_map_of_x = rv.logpdf(map_of_x)
-    log_det = map.LogDeterminant(x.reshape(1,num_points))
+    tri_map.SetCoeffs(coeffs)
+    map_of_x = tri_map.Evaluate(x)
+    rho_of_map_of_x = rv.logpdf(map_of_x.T)
+    log_det = tri_map.LogDeterminant(x)
     return -np.sum(rho_of_map_of_x + log_det)/num_points
+
+objective(coeffs)
 
 # Optimize
 print('Starting coeffs')
-print(map.CoeffMap())
-print('and error: {:.2E}'.format(objective(map.CoeffMap())))
-res = minimize(objective, map.CoeffMap(), method="Nelder-Mead")
+print(tri_map.CoeffMap())
+print('and error: {:.2E}'.format(objective(tri_map.CoeffMap())))
+res = minimize(objective, tri_map.CoeffMap(), method="Nelder-Mead")
 print('Final coeffs')
-print(map.CoeffMap())
-print('and error: {:.2E}'.format(objective(map.CoeffMap())))
+print(tri_map.CoeffMap())
+print('and error: {:.2E}'.format(objective(tri_map.CoeffMap())))
 
-# After optimization plot
-map_of_x = map.Evaluate(x.reshape(1,num_points))
+# Before optimization
+map_of_x = tri_map.Evaluate(x) 
 plt.figure()
-plt.hist(map_of_x.reshape(num_points,), num_bins, facecolor='blue', alpha=0.5, density=True, label='Normalized samples')
-plt.plot(t,rho_t,label="Reference density")
+#plt.hist2d(x[0],x[1], num_bins, facecolor='blue', alpha=0.5, density=True, label='Target samples')
+plt.contour(T1,T2,rho_t,label="Reference density")
+plt.scatter(map_of_x[0],map_of_x[1], facecolor='blue', alpha=0.1, label='Target samples')
 plt.legend()
 plt.show()
