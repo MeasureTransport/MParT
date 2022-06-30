45678ab73d6de6de0593690030d7f50b4511f9ad
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2022-06-30 11:12:30 -0400

    replaced unittest by pytest

--- bindings/python/tests/test_FixedMultiIndexSet.py
+++ bindings/python/tests/test_FixedMultiIndexSet.py
@@ -2,15 +2,9 @@
 
 import mpart
 import numpy as np
-import unittest
 
-class TestFixedMsetMethods(unittest.TestCase):
-
-    def test_max_degrees(self):
-        multis = np.array([[0, 1], [2, 0]])
-        mset = mpart.MultiIndexSet(multis)
-        fixed_mset = mset.fix(True)
-        assert np.all(fixed_mset == [1,2])
-
-if __name__ == '__main__':
-    unittest.main()
+def test_max_degrees():
+    multis = np.array([[0, 1], [2, 0]])
+    mset = mpart.MultiIndexSet(multis)
+    fixed_mset = mset.fix(True)
+    assert np.all(fixed_mset == [1,2])
