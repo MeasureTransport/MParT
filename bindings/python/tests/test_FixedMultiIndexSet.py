9fdb8de1ee3997b07a8fb8da58f1489da8eefcf7
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2022-06-30 11:16:59 -0400

    fixed assert

--- bindings/python/tests/test_FixedMultiIndexSet.py
+++ bindings/python/tests/test_FixedMultiIndexSet.py
@@ -7,4 +7,4 @@ def test_max_degrees():
     multis = np.array([[0, 1], [2, 0]])
     mset = mpart.MultiIndexSet(multis)
     fixed_mset = mset.fix(True)
-    assert np.all(fixed_mset == [1,2])
+    assert np.all(fixed_mset.MaxDegrees() == [2,1])
