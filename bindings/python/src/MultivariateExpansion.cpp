031330a951d1cf9ea3a61e5a527157b36f2fe46a
Michael C Brennan <mcbrenn@mit.edu>
2022-07-08 07:17:47 -0400

    Python bindings for MultivariateExpansion compiles but doesn't do much yet

--- bindings/python/src/MultivariateExpansion.cpp
+++ bindings/python/src/MultivariateExpansion.cpp
@@ -1,5 +1,6 @@
 #include "CommonPybindUtilities.h"
 #include "MParT/MultivariateExpansion.h"
+#include "MParT/OrthogonalPolynomial.h"
 #include "MParT/MultiIndices/MultiIndexSet.h"
 #include "MParT/MultiIndices/FixedMultiIndexSet.h"
 #include <pybind11/stl.h>
@@ -13,10 +14,11 @@ using namespace mpart::binding;
 
 void mpart::binding::MultivariateExpansionWrapper(py::module &m)
 {
-    py::class_<MultivariateExpansion<BasisEvaluatorType, Kokkos::HostSpace>, KokkosCustomPointer<MultivariateExpansion>>(m, "MultivariateExpansion")
+    py::class_<MultivariateExpansion<ProbabilistHermite,Kokkos::HostSpace>>(m, "MultivariateExpansion")
         // MultivariateExpansion
         .def(py::init<MultiIndexSet const&>())
-        .def("Evaluate", &MultivariateExpansion::Evaluate)
+        .def("NumCoeffs", &MultivariateExpansion<ProbabilistHermite,Kokkos::HostSpace>::NumCoeffs)
+        //.def("Evaluate", &MultivariateExpansion<ProbabilistHermite,Kokkos::HostSpace>::Evaluate)
         ;
 
 }
