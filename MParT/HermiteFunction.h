f5afb47a55a077877e4b1e73e42dadc5f02f537e
Matthew Parno <parnomd@gmail.com>
2022-07-11 12:42:44 -0400

    Removed tgamma use in HermiteFunction.

--- MParT/HermiteFunction.h
+++ MParT/HermiteFunction.h
@@ -15,13 +15,16 @@ public:
                      unsigned int         maxOrder,
                      double               x) const
     {   
+        
         // Evaluate all of the physicist hermite polynomials
-        polyBase.EvaluateAll(output, maxOrder, x);
+        output[0] = std::pow(M_PI, -0.25) * std::exp(-0.5*x*x);
 
-        // Add the scaling 
-        const double baseScaling = std::pow(M_PI, -0.25) * std::exp(-0.5*x*x);
-        for(unsigned int i=0; i<=maxOrder; ++i)
-            output[i] *= (baseScaling * std::pow( std::pow(2, i) * std::tgamma(i+1), -0.5) );
+        if(maxOrder>0){
+            output[1] = std::sqrt(2.0) * x * output[0];
+
+            for(unsigned int i=2; i<=maxOrder; ++i)
+                output[i] = (x*output[i-1]  - std::sqrt(0.5*(i-1))*output[i-2])/std::sqrt(0.5*i);
+        }
     }
 
    KOKKOS_INLINE_FUNCTION  void EvaluateDerivatives(double*              vals,
@@ -29,14 +32,23 @@ public:
                              unsigned int         maxOrder,
                              double               x) const
     {   
+
         // Evaluate all of the physicist hermite polynomials
         polyBase.EvaluateDerivatives(vals, derivs, maxOrder, x);
 
         // Add the scaling 
         const double baseScaling = std::pow(M_PI, -0.25) * std::exp(-0.5*x*x);
         double scale;
-        for(unsigned int i=0; i<=maxOrder; ++i){
-            scale = baseScaling * std::pow( std::pow(2, i) * std::tgamma(i+1), -0.5);
+        double currFactorial = 1;
+
+        scale = baseScaling;
+        derivs[0] -= x*vals[0];
+        derivs[0] *= scale;
+        vals[0] *= scale;
+
+        for(unsigned int i=1; i<=maxOrder; ++i){
+            currFactorial *= i;
+            scale = baseScaling * std::pow( std::pow(2, i) * currFactorial, -0.5);
             derivs[i] -= x*vals[i];
             derivs[i] *= scale;
             vals[i] *= scale;
@@ -62,7 +74,7 @@ public:
     KOKKOS_INLINE_FUNCTION double Evaluate(unsigned int const order, 
                     double const x) const
     {
-        const double scaling = std::pow( std::pow(2, order) * std::tgamma(order+1) *std::sqrt(M_PI), -0.5);
+        const double scaling = std::pow( std::pow(2, order) * Factorial(order) *std::sqrt(M_PI), -0.5);
 
         return scaling * std::exp(-0.5*x*x) * polyBase.Evaluate(order, x);
 
@@ -71,7 +83,7 @@ public:
     KOKKOS_INLINE_FUNCTION double Derivative(unsigned int const order, 
                       double const x) const 
     {
-        const double scaling = std::pow( std::pow(2, order) * std::tgamma(order+1) *std::sqrt(M_PI), -0.5);
+        const double scaling = std::pow( std::pow(2, order) * Factorial(order) *std::sqrt(M_PI), -0.5);
         const double expPart = std::exp(-0.5*x*x);
         return scaling * ( -x*expPart*polyBase.Evaluate(order, x) + expPart * polyBase.Derivative(order,x) );
     }
@@ -85,6 +97,13 @@ public:
 private:
     PhysicistHermite polyBase;
 
+    KOKKOS_INLINE_FUNCTION unsigned int Factorial(unsigned int d) const{
+        unsigned int out = 1;
+        for(unsigned int i=2; i<=d; ++i)
+            out *= i;
+        return out;
+    }
+    
 }; // class HermiteFunction
 
 }
