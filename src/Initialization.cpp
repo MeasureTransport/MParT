04ca3c53b02e6a4f60f2c14f9cb3106c3a935807
Matthew Parno <parnomd@gmail.com>
2022-07-13 16:53:45 -0400

    More print statements.

--- src/Initialization.cpp
+++ src/Initialization.cpp
@@ -5,7 +5,9 @@ using namespace mpart;
 
 void mpart::Finalize()
 {
+    std::cout << "Before kokkos::finalize..." << std::endl;
     Kokkos::finalize();
+    std::cout << "After kokkos::finalize..." << std::endl;
 }
 
 InitializeStatus& mpart::GetInitializeStatusObject()
