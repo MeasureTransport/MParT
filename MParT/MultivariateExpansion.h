3827b9f2a3a510f3c0327ecfb1e59df36a47d8a7
Matthew Parno <parnomd@gmail.com>
2022-07-13 17:28:52 -0400

    Removing vector of workers.

--- MParT/MultivariateExpansion.h
+++ MParT/MultivariateExpansion.h
@@ -7,8 +7,6 @@
 
 #include <algorithm>
 
-#include <Kokkos_Vector.hpp>
-
 namespace mpart{
 
     /**
@@ -27,47 +25,35 @@ namespace mpart{
         template<typename SetType>
         MultivariateExpansion(unsigned int              outDim, 
                               SetType            const& mset, 
-                              BasisEvaluatorType const& basis1d) : ParameterizedFunctionBase<MemorySpace>(mset.Length(), outDim, mset.Size()*outDim)
-        {   std::cout << "Here 0..." << std::endl;
-            workers.push_back( MultivariateExpansionWorker<BasisEvaluatorType, MemorySpace>(mset, basis1d) );
-            std::cout << "Here 1..." << std::endl;
+                              BasisEvaluatorType const& basis1d) : ParameterizedFunctionBase<MemorySpace>(mset.Length(), outDim, mset.Size()*outDim),
+                                                                   worker(mset, basis1d)
+        {   
         };
 
         template<typename ExpansionType>
         MultivariateExpansion(unsigned int  outDim, 
-                              ExpansionType expansion) : ParameterizedFunctionBase<MemorySpace>(expansion.InputSize(), outDim, expansion.NumCoeffs()*outDim)
+                              ExpansionType expansion) : ParameterizedFunctionBase<MemorySpace>(expansion.InputSize(), outDim, expansion.NumCoeffs()*outDim),
+                                                         worker(expansion)
         {
-            workers.push_back( expansion );
         };
 
-        // template<typename SetType>
-        // MultivariateExpansion(std::vector<SetType> const& msets, 
-        //                       BasisEvaluatorType   const& basis1d) : ParameterizedFunctionBase<MemorySpace>(msets.at(0).Length(),  // input dimension
-        //                                                                                        msets.size(),          // output dimension
-        //                                                                                        std::accumulate(msets.begin(), msets.end(), 0, [](size_t sum, const SetType& mset){ return sum + mset.Size(); })) // number of coefficients
-        // {
-        //     for(unsigned int i=0; i<msets.size(); ++i)
-        //         workers.push_back( MultivariateExpansionWorker<BasisEvaluatorType, MemorySpace>(msets.at(i), basis1d) );
-        // }
-
-        virtual ~MultivariateExpansion(){std::cout << "In destructor..." << std::endl;};
+        virtual ~MultivariateExpansion(){
+            std::cout << "End of destructor..." << std::endl;
+        };
 
 
         virtual void EvaluateImpl(Kokkos::View<const double**, MemorySpace> const& pts,
                                   Kokkos::View<double**, MemorySpace>            & output) override
         {
             using ExecutionSpace = typename MemoryToExecution<MemorySpace>::Space;
-            assert(workers.size()>0);
-
+            
             const unsigned int numPts = pts.extent(1);
 
             // Figure out how much memory we'll need in the cache
-            unsigned int cacheSize = workers[0].CacheSize();
-            for(unsigned int i=1; i<workers.size(); ++i)
-                cacheSize = std::max(cacheSize, workers[i].CacheSize());
+            unsigned int cacheSize = worker.CacheSize();
 
             // Define functor if there is a constant worker for all dimensions
-            auto functor1 = KOKKOS_CLASS_LAMBDA (typename Kokkos::TeamPolicy<ExecutionSpace>::member_type team_member) {
+            auto functor = KOKKOS_CLASS_LAMBDA (typename Kokkos::TeamPolicy<ExecutionSpace>::member_type team_member) {
 
                 unsigned int ptInd = team_member.league_rank () * team_member.team_size () + team_member.team_rank ();
 
@@ -80,52 +66,20 @@ namespace mpart{
                     Kokkos::View<double*,MemorySpace> cache(team_member.thread_scratch(1), cacheSize);
 
                     // Fill in entries in the cache that are independent of x_d.  By passing DerivativeFlags::None, we are telling the expansion that no derivatives with wrt x_1,...x_{d-1} will be needed.
-                    workers[0].FillCache1(cache.data(), pt, DerivativeFlags::None);
-                    workers[0].FillCache2(cache.data(), pt, pt(pt.size()-1), DerivativeFlags::None);
+                    worker.FillCache1(cache.data(), pt, DerivativeFlags::None);
+                    worker.FillCache2(cache.data(), pt, pt(pt.size()-1), DerivativeFlags::None);
 
                     unsigned int coeffStartInd = 0;
                     
                     for(unsigned int d=0; d<this->outputDim; ++d){
 
                         // Extract the coefficients for this output dimension
-                        auto coeffs = Kokkos::subview(this->savedCoeffs, std::make_pair(coeffStartInd, coeffStartInd+workers[0].NumCoeffs()));
+                        auto coeffs = Kokkos::subview(this->savedCoeffs, std::make_pair(coeffStartInd, coeffStartInd+worker.NumCoeffs()));
                          
                         // Evaluate the expansion
-                        output(d,ptInd) = workers[0].Evaluate(cache.data(), coeffs);
+                        output(d,ptInd) = worker.Evaluate(cache.data(), coeffs);
 
-                        coeffStartInd += workers[0].NumCoeffs();
-                    }
-                }
-            };
-
-            // Define functor for case where different workers are used in each dimension
-            auto functor2 = KOKKOS_CLASS_LAMBDA (typename Kokkos::TeamPolicy<ExecutionSpace>::member_type team_member) {
-
-                unsigned int ptInd = team_member.league_rank () * team_member.team_size () + team_member.team_rank ();
-
-                if(ptInd<numPts){
-
-                    // Create a subview containing only the current point
-                    auto pt = Kokkos::subview(pts, Kokkos::ALL(), ptInd);
-                    
-                    // Get a pointer to the shared memory that Kokkos set up for this team
-                    Kokkos::View<double*,MemorySpace> cache(team_member.thread_scratch(1), cacheSize);
-
-                    unsigned int coeffStartInd = 0;
-
-                    for(unsigned int d=0; d<this->outputDim; ++d){
-
-                        // Extract the coefficients for this output dimension
-                        auto coeffs = Kokkos::subview(this->savedCoeffs, std::make_pair(coeffStartInd, coeffStartInd+workers[d].NumCoeffs()));
-                         
-                        // Fill in entries in the cache that are independent of x_d.  By passing DerivativeFlags::None, we are telling the expansion that no derivatives with wrt x_1,...x_{d-1} will be needed.
-                        workers[d].FillCache1(cache.data(), pt, DerivativeFlags::None);
-                        workers[d].FillCache2(cache.data(), pt, pt(pt.size()-1), DerivativeFlags::None);
-
-                        // Evaluate the expansion
-                        output(d,ptInd) = workers[d].Evaluate(cache.data(), coeffs);
-
-                        coeffStartInd += workers[d].NumCoeffs();
+                        coeffStartInd += worker.NumCoeffs();
                     }
                 }
             };
@@ -134,24 +88,14 @@ namespace mpart{
             Kokkos::TeamPolicy<ExecutionSpace> policy;
             policy.set_scratch_size(1,Kokkos::PerTeam(0), Kokkos::PerThread(cacheBytes));
             
-            if(workers.size()==1){
-                const unsigned int threadsPerTeam = std::min<unsigned int>(numPts, policy.team_size_recommended(functor1, Kokkos::ParallelForTag()));
-                const unsigned int numTeams = std::ceil( double(numPts) / threadsPerTeam );
-                
-                policy = Kokkos::TeamPolicy<ExecutionSpace>(numTeams, threadsPerTeam).set_scratch_size(1,Kokkos::PerTeam(0), Kokkos::PerThread(cacheBytes));
+            const unsigned int threadsPerTeam = std::min<unsigned int>(numPts, policy.team_size_recommended(functor, Kokkos::ParallelForTag()));
+            const unsigned int numTeams = std::ceil( double(numPts) / threadsPerTeam );
+            
+            policy = Kokkos::TeamPolicy<ExecutionSpace>(numTeams, threadsPerTeam).set_scratch_size(1,Kokkos::PerTeam(0), Kokkos::PerThread(cacheBytes));
 
-                // Paralel loop over each point computing T(x_1,...,x_D) for that point
-                Kokkos::parallel_for(policy, functor1);
-            }else{
-                const unsigned int threadsPerTeam = std::min<unsigned int>(numPts, policy.team_size_recommended(functor2, Kokkos::ParallelForTag()));
-                const unsigned int numTeams = std::ceil( double(numPts) / threadsPerTeam );
-                
-                policy = Kokkos::TeamPolicy<ExecutionSpace>(numTeams, threadsPerTeam).set_scratch_size(1,Kokkos::PerTeam(0), Kokkos::PerThread(cacheBytes));
-
-                // Paralel loop over each point computing T(x_1,...,x_D) for that point
-                Kokkos::parallel_for(policy, functor2);
-            }
-
+            // Paralel loop over each point computing T(x_1,...,x_D) for that point
+            Kokkos::parallel_for(policy, functor);
+    
             Kokkos::fence();
         }
 
@@ -160,20 +104,15 @@ namespace mpart{
                            Kokkos::View<double**, MemorySpace>            & output) override
         {
             using ExecutionSpace = typename MemoryToExecution<MemorySpace>::Space;
-            assert(workers.size()>0);
-
+            
             const unsigned int numPts = pts.extent(1);
 
             // Figure out how much memory we'll need in the cache
-            unsigned int cacheSize = workers[0].CacheSize();
-            unsigned int maxParams = workers[0].NumCoeffs();
+            unsigned int cacheSize = worker.CacheSize();
+            unsigned int maxParams = worker.NumCoeffs();
 
-            for(unsigned int i=1; i<workers.size(); ++i){
-                cacheSize = std::max(cacheSize, workers[i].CacheSize());
-                maxParams = std::max(maxParams, workers[i].NumCoeffs());
-            }
 
-            auto functor1 = KOKKOS_CLASS_LAMBDA (typename Kokkos::TeamPolicy<ExecutionSpace>::member_type team_member) {
+            auto functor = KOKKOS_CLASS_LAMBDA (typename Kokkos::TeamPolicy<ExecutionSpace>::member_type team_member) {
 
                 unsigned int ptInd = team_member.league_rank () * team_member.team_size () + team_member.team_rank ();
 
@@ -187,91 +126,46 @@ namespace mpart{
                     Kokkos::View<double*,MemorySpace> grad(team_member.thread_scratch(1), maxParams);
 
                     // Fill in entries in the cache that are independent of x_d.  By passing DerivativeFlags::None, we are telling the expansion that no derivatives with wrt x_1,...x_{d-1} will be needed.
-                    workers[0].FillCache1(cache.data(), pt, DerivativeFlags::Parameters);
-                    workers[0].FillCache2(cache.data(), pt, pt(pt.size()-1), DerivativeFlags::Parameters);
+                    worker.FillCache1(cache.data(), pt, DerivativeFlags::Parameters);
+                    worker.FillCache2(cache.data(), pt, pt(pt.size()-1), DerivativeFlags::Parameters);
 
                     unsigned int coeffStartInd = 0;
 
                     for(unsigned int d=0; d<this->outputDim; ++d){
 
                         // Extract the coefficients for this output dimension
-                        auto coeffs = Kokkos::subview(this->savedCoeffs, std::make_pair(coeffStartInd, coeffStartInd+workers[0].NumCoeffs()));
+                        auto coeffs = Kokkos::subview(this->savedCoeffs, std::make_pair(coeffStartInd, coeffStartInd+worker.NumCoeffs()));
                          
                         // Evaluate the expansion
-                        workers[0].CoeffDerivative(cache.data(), coeffs, grad);
+                        worker.CoeffDerivative(cache.data(), coeffs, grad);
 
-                        for(unsigned int i=0; i<workers[0].NumCoeffs(); ++i)
+                        for(unsigned int i=0; i<worker.NumCoeffs(); ++i)
                             output(coeffStartInd + i, ptInd) = sens(d,ptInd) * grad(i);
 
-                        coeffStartInd += workers[0].NumCoeffs();
+                        coeffStartInd += worker.NumCoeffs();
                     }
                 }
             };
 
-            auto functor2 = KOKKOS_CLASS_LAMBDA (typename Kokkos::TeamPolicy<ExecutionSpace>::member_type team_member) {
-
-                unsigned int ptInd = team_member.league_rank () * team_member.team_size () + team_member.team_rank ();
-
-                if(ptInd<numPts){
-
-                    // Create a subview containing only the current point
-                    auto pt = Kokkos::subview(pts, Kokkos::ALL(), ptInd);
-                    
-                    // Get a pointer to the shared memory that Kokkos set up for this team
-                    Kokkos::View<double*,MemorySpace> cache(team_member.thread_scratch(1), cacheSize);
-                    Kokkos::View<double*,MemorySpace> grad(team_member.thread_scratch(1), maxParams);
-
-                    unsigned int coeffStartInd = 0;
-                    
-                    for(unsigned int d=0; d<this->outputDim; ++d){
-
-                        // Extract the coefficients for this output dimension
-                        auto coeffs = Kokkos::subview(this->savedCoeffs, std::make_pair(coeffStartInd, coeffStartInd+workers[d].NumCoeffs()));
-                         
-                        // Fill in entries in the cache that are independent of x_d.  By passing DerivativeFlags::None, we are telling the expansion that no derivatives with wrt x_1,...x_{d-1} will be needed.
-                        workers[d].FillCache1(cache.data(), pt, DerivativeFlags::Parameters);
-                        workers[d].FillCache2(cache.data(), pt, pt(pt.size()-1), DerivativeFlags::Parameters);
-
-                        // Evaluate the expansion
-                        workers[d].CoeffDerivative(cache.data(), coeffs, grad);
-
-                        for(unsigned int i=0; i<workers[d].NumCoeffs(); ++i)
-                            output(coeffStartInd + i, ptInd) = sens(d,ptInd) * grad(i);
-
-                        coeffStartInd += workers[d].NumCoeffs();
-                    }
-                }
-            };
 
             auto cacheBytes = Kokkos::View<double*,MemorySpace>::shmem_size(cacheSize + maxParams);
             Kokkos::TeamPolicy<ExecutionSpace> policy;
             policy.set_scratch_size(1,Kokkos::PerTeam(0), Kokkos::PerThread(cacheBytes));
 
-            if(workers.size()==1){
-                const unsigned int threadsPerTeam = std::min<unsigned int>(numPts, policy.team_size_recommended(functor1, Kokkos::ParallelForTag()));
-                const unsigned int numTeams = std::ceil( double(numPts) / threadsPerTeam );
-                
-                policy = Kokkos::TeamPolicy<ExecutionSpace>(numTeams, threadsPerTeam).set_scratch_size(1,Kokkos::PerTeam(0), Kokkos::PerThread(cacheBytes));
+            const unsigned int threadsPerTeam = std::min<unsigned int>(numPts, policy.team_size_recommended(functor, Kokkos::ParallelForTag()));
+            const unsigned int numTeams = std::ceil( double(numPts) / threadsPerTeam );
+            
+            policy = Kokkos::TeamPolicy<ExecutionSpace>(numTeams, threadsPerTeam).set_scratch_size(1,Kokkos::PerTeam(0), Kokkos::PerThread(cacheBytes));
 
-                // Paralel loop over each point computing T(x_1,...,x_D) for that point
-                Kokkos::parallel_for(policy, functor1);
-
-            }else{
-                const unsigned int threadsPerTeam = std::min<unsigned int>(numPts, policy.team_size_recommended(functor2, Kokkos::ParallelForTag()));
-                const unsigned int numTeams = std::ceil( double(numPts) / threadsPerTeam );
-                
-                policy = Kokkos::TeamPolicy<ExecutionSpace>(numTeams, threadsPerTeam).set_scratch_size(1,Kokkos::PerTeam(0), Kokkos::PerThread(cacheBytes));
-
-                // Paralel loop over each point computing T(x_1,...,x_D) for that point
-                Kokkos::parallel_for(policy, functor2);
-            }
+            // Paralel loop over each point computing T(x_1,...,x_D) for that point
+            Kokkos::parallel_for(policy, functor);
 
             Kokkos::fence();
         }
 
     private:
 
-        Kokkos::vector< MultivariateExpansionWorker<BasisEvaluatorType, MemorySpace> > workers;
+        MultivariateExpansionWorker<BasisEvaluatorType, MemorySpace> worker;
 
     }; // class MultivariateExpansion
 }
