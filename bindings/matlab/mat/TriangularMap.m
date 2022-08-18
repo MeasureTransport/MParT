61dcaef9897d67b63b449096c2c0eb1ec73469c6
rubiop <paulbaptiste.rubio@gmail.com>
2022-08-18 19:52:09 -0400

    forgot semicolon

--- bindings/matlab/mat/TriangularMap.m
+++ bindings/matlab/mat/TriangularMap.m
@@ -1,7 +1,7 @@
 function map = TriangularMap(listCondMaps)
     listCondMap_ids =[];
     for i=1:length(listCondMaps)
-        listCondMap_ids=[listCondMap_ids,listCondMaps(i).get_id()]
+        listCondMap_ids=[listCondMap_ids,listCondMaps(i).get_id()];
     end
     map = ConditionalMap(listCondMap_ids);
 end
