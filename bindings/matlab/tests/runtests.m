6443ed9626c3cbf53e873b80e9b32ca932034248
Danny Sharp <dannys4@mit.edu>
2022-11-17 10:19:56 -0500

    Initialize MParT

--- bindings/matlab/tests/runtests.m
+++ bindings/matlab/tests/runtests.m
@@ -2,6 +2,8 @@ import matlab.unittest.TestRunner
 import matlab.unittest.TestSuite
 import matlab.unittest.plugins.XMLPlugin
 
+KokkosInitialize(2);
+
 suite = TestSuite.fromClass(?FixedMultiIndexSetTest);
 runner = TestRunner.withNoPlugins;
 xmlFile = 'test-results-matlab.xml';
