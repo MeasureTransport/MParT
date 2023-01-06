ba06361f2ae09137528a372af6464cebb47d0428
Danny Sharp <dannys4@mit.edu>
2023-01-06 16:41:18 -0500

    Working pushforward, pullback logdensity

--- tests/Distributions/Test_TransportDensity.cpp
+++ tests/Distributions/Test_TransportDensity.cpp
@@ -13,13 +13,19 @@ using namespace Catch;
 TEST_CASE( "Testing Pullback/Pushforward density", "[PullbackPushforwardDensity]") {
     unsigned int dim = 2;
     unsigned int N_samp = 1000;
+
+    double diag_el = 2.0;
     Kokkos::View<double**, Kokkos::HostSpace> A("A", dim, dim);
     Kokkos::View<double*, Kokkos::HostSpace> b("b", dim);
     for(int i = 0; i < dim; i++) {
         b(i) = 1.0;
         for(int j = 0; j < dim; j++) {
-            A(i, j) = ((double) i == j)*2.0;
+            A(i, j) = ((double) i == j)*diag_el;
         }
+    }
+    double logdet = 0.;
+    for(int i = 0; i < dim; i++) {
+        logdet += std::log(diag_el);
     }
     auto map = std::make_shared<AffineMap<Kokkos::HostSpace>>(A, b);
     auto density = std::make_shared<GaussianSamplerDensity<Kokkos::HostSpace>>(dim);
@@ -27,7 +33,7 @@ TEST_CASE( "Testing Pullback/Pushforward density", "[PullbackPushforwardDensity]
     PushforwardDensity<Kokkos::HostSpace> pushforward {map, density};
     density->SetSeed(0);
     StridedMatrix<const double, Kokkos::HostSpace> samples = density->Sample(N_samp);
-    double offset = -0.9189385332046727; // -log(2*pi)
+    double offset = -0.9189385332046727; // -log(2*pi)/2
     offset *= dim;
     auto logPullbackDensitySample = pullback.LogDensity(samples);
     auto logPushforwardDensitySample = pushforward.LogDensity(samples);
@@ -35,18 +41,19 @@ TEST_CASE( "Testing Pullback/Pushforward density", "[PullbackPushforwardDensity]
     Kokkos::View<double**, Kokkos::HostSpace> nullPrefix ("null prefix", 0, N_samp);
     auto pushforwardEvalSample = map->Inverse(nullPrefix, samples);
     Kokkos::parallel_for("TestPullbackDensity", N_samp, KOKKOS_LAMBDA(const int i) {
+        // Take the norm of the pullback and pushforward evaluations
         double sampleNormPullback = 0.0;
         double sampleNormPushforward = 0.0;
-
         for(int j = 0; j < dim; j++) {
             sampleNormPullback += pullbackEvalSample(j, i)*pullbackEvalSample(j, i);
             sampleNormPushforward += pushforwardEvalSample(j, i)*pushforwardEvalSample(j, i);
         }
 
-        logPullbackDensitySample(i) -= -0.5*sampleNormPullback + offset;
+        // Calculate the pullback and pushforward density error inplace
+        logPullbackDensitySample(i) -= -0.5*sampleNormPullback + offset + logdet;
         logPullbackDensitySample(i) = std::abs(logPullbackDensitySample(i));
 
-        logPushforwardDensitySample(i) -= -0.5*sampleNormPushforward + offset;
+        logPushforwardDensitySample(i) -= -0.5*sampleNormPushforward + offset + logdet;
         logPushforwardDensitySample(i) = std::abs(logPushforwardDensitySample(i));
     });
     double max_pullback_err = *std::max_element(logPullbackDensitySample.data(), logPullbackDensitySample.data()+N_samp);
