e2ea18eef87329053784f7dd3011aac984c211fb
Danny Sharp <dannys4@mit.edu>
2023-01-09 16:07:27 -0500

    Add CoeffGradLogDensity on pullback

--- MParT/Distributions/PullbackDensity.h
+++ MParT/Distributions/PullbackDensity.h
@@ -1,6 +1,7 @@
 #ifndef MPART_PULLBACKDENSITY_H
 #define MPART_PULLBACKDENSITY_H
 
+#include "MParT/Utilities/ArrayConversions.h"
 #include "MParT/Distributions/DensityBase.h"
 #include "MParT/Utilities/LinearAlgebra.h"
 #include "MParT/ConditionalMapBase.h"
@@ -12,30 +13,17 @@ class PullbackDensity: public DensityBase<MemorySpace> {
 
     public:
     PullbackDensity() = delete;
-    PullbackDensity(std::shared_ptr<ConditionalMapBase<MemorySpace>> map, std::shared_ptr<DensityBase<MemorySpace>> reference):
-        DensityBase<MemorySpace>(reference->Dim()), map_(map), reference_(reference) {
-        if (map_->outputDim != reference_->Dim()) {
-            throw std::invalid_argument("PullbackDensity: map output dimension does not match reference density dimension");
-        }
-        if (map_->inputDim != map_->outputDim) {
-            throw std::invalid_argument("PullbackDensity: map input dimension does not match map output dimension");
-        }
-    }
+    PullbackDensity(std::shared_ptr<ConditionalMapBase<MemorySpace>> map, std::shared_ptr<DensityBase<MemorySpace>> reference);
 
-    void LogDensityImpl(StridedMatrix<const double, MemorySpace> const &pts, StridedVector<double, MemorySpace> output) override {
-        StridedMatrix<double, MemorySpace> mappedPts = map_->Evaluate(pts);
-        reference_->LogDensityImpl(mappedPts, output);
-        StridedVector<double, MemorySpace> logJacobian = map_->LogDeterminant(pts);
-        output += logJacobian;
-    };
+    void LogDensityImpl(StridedMatrix<const double, MemorySpace> const &pts, StridedVector<double, MemorySpace> output) override;
 
-    void GradLogDensityImpl(StridedMatrix<const double, MemorySpace> const &pts, StridedMatrix<double, MemorySpace> output) override {
-        StridedMatrix<const double, MemorySpace> mappedPts = map_->Evaluate(pts);
-        StridedMatrix<double, MemorySpace> sens_map = reference_->GradLogDensity(mappedPts);
-        map_->GradientImpl(pts, sens_map, output);
-        StridedMatrix<double, MemorySpace> gradLogJacobian = map_->LogDeterminantInputGrad(pts);
-        output += gradLogJacobian;
-    };
+    void GradLogDensityImpl(StridedMatrix<const double, MemorySpace> const &pts, StridedMatrix<double, MemorySpace> output) override;
+
+    void CoeffGradLogDensityImpl(StridedMatrix<const double, MemorySpace> const &pts, StridedMatrix<double, MemorySpace> output);
+
+    StridedMatrix<double, MemorySpace> CoeffGradLogDensity(StridedMatrix<const double, MemorySpace> const &pts);
+
+    Eigen::RowMatrixXd CoeffGradLogDensity(Eigen::Ref<const Eigen::RowMatrixXd> const &pts);
 
     private:
     std::shared_ptr<ConditionalMapBase<MemorySpace>> map_;
---
+++ src/Distributions/PullbackDensity.cpp
@@ -0,0 +1,63 @@
+#include "MParT/Distributions/PullbackDensity.h"
+
+using namespace mpart;
+
+template<typename MemorySpace>
+PullbackDensity<MemorySpace>::PullbackDensity(std::shared_ptr<ConditionalMapBase<MemorySpace>> map, std::shared_ptr<DensityBase<MemorySpace>> reference):
+    DensityBase<MemorySpace>(reference->Dim()), map_(map), reference_(reference) {
+    if (map_->outputDim != reference_->Dim()) {
+        throw std::invalid_argument("PullbackDensity: map output dimension does not match reference density dimension");
+    }
+    if (map_->inputDim != map_->outputDim) {
+        throw std::invalid_argument("PullbackDensity: map input dimension does not match map output dimension");
+    }
+}
+
+template<typename MemorySpace>
+void PullbackDensity<MemorySpace>::LogDensityImpl(StridedMatrix<const double, MemorySpace> const &pts, StridedVector<double, MemorySpace> output) {
+    StridedMatrix<double, MemorySpace> mappedPts = map_->Evaluate(pts);
+    reference_->LogDensityImpl(mappedPts, output);
+    StridedVector<double, MemorySpace> logJacobian = map_->LogDeterminant(pts);
+    output += logJacobian;
+}
+
+template<typename MemorySpace>
+void PullbackDensity<MemorySpace>::GradLogDensityImpl(StridedMatrix<const double, MemorySpace> const &pts, StridedMatrix<double, MemorySpace> output) {
+    StridedMatrix<const double, MemorySpace> mappedPts = map_->Evaluate(pts);
+    StridedMatrix<double, MemorySpace> sens_map = reference_->GradLogDensity(mappedPts);
+    map_->GradientImpl(pts, sens_map, output);
+    StridedMatrix<double, MemorySpace> gradLogJacobian = map_->LogDeterminantInputGrad(pts);
+    output += gradLogJacobian;
+}
+
+template<typename MemorySpace>
+void PullbackDensity<MemorySpace>::CoeffGradLogDensityImpl(StridedMatrix<const double, MemorySpace> const &pts, StridedMatrix<double, MemorySpace> output) {
+    StridedMatrix<const double, MemorySpace> mappedPts = map_->Evaluate(pts);
+    StridedMatrix<double, MemorySpace> sens_map = reference_->GradLogDensity(mappedPts);
+    map_->CoeffGradImpl(pts, sens_map, output);
+    StridedMatrix<double, MemorySpace> gradLogJacobian = map_->LogDeterminantCoeffGrad(pts);
+    output += gradLogJacobian;
+}
+
+template<typename MemorySpace>
+StridedMatrix<double, MemorySpace> PullbackDensity<MemorySpace>::CoeffGradLogDensity(StridedMatrix<const double, MemorySpace> const &pts) {
+    Kokkos::View<double**, MemorySpace> output("CoeffGradLogDensity", map_->numCoeffs, pts.extent(1));
+    CoeffGradLogDensityImpl(pts, output);
+    return output;
+}
+
+template<>
+Eigen::RowMatrixXd PullbackDensity<Kokkos::HostSpace>::CoeffGradLogDensity(Eigen::Ref<const Eigen::RowMatrixXd> const &pts) {
+    // Allocate output
+    Eigen::RowMatrixXd output(pts.rows(), pts.cols());
+    StridedMatrix<const double, Kokkos::HostSpace> ptsView = ConstRowMatToKokkos<double, Kokkos::HostSpace>(pts);
+    StridedMatrix<double, Kokkos::HostSpace> outView = MatToKokkos<double, Kokkos::HostSpace>(output);
+    // Call the LogDensity function
+    GradLogDensityImpl(ptsView, outView);
+    return output;
+}
+
+template class mpart::PullbackDensity<Kokkos::HostSpace>;
+#if defined(MPART_ENABLE_GPU)
+    template class mpart::PullbackDensity<mpart::DeviceSpace>;
+#endif
