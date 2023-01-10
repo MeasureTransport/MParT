0316cb071f7d41d1e671369d257401393d170bce
Danny Sharp <dannys4@mit.edu>
2023-01-10 16:14:22 -0500

    Move ReduceColumn

--- MParT/Utilities/MathFunctions.h
+++ MParT/Utilities/MathFunctions.h
@@ -15,39 +15,6 @@ namespace mpart{
         return out;
     }
 
-    // This struct allows you to reduce the columns of a mxn matrix
-    // Performs alpha*A*[1,...,1]
-    template<typename MemorySpace>
-    struct ReduceColumn {
-        using value_type = double*;
-        using size_type = Kokkos::View<double**, MemorySpace>::size_type;
-
-        // Keep track of the row count, m
-        size_type value_count;
-
-        Kokkos::View<double**, MemorySpace> A_;
-        double alpha_;
-
-        ReduceColumn(Kokkos::View<double**, MemorySpace> A, double alpha): value_count(A.extent(0)), A_(A), alpha_(alpha) {}
-
-        KOKKOS_INLINE_FUNCTION void opterator()(const size_type j, value_type sum) const {
-            for(size_type i=0; i<value_count; ++i)
-                sum[i] += A_(i,j)*alpha_;
-        }
-
-        KOKKOS_INLINE_FUNCTION void join (volatile value_type dst, const volatile value_type src) const {
-            for (size_type i = 0; i < value_count; ++i) {
-                dst[i] += src[i];
-            }
-        }
-
-        KOKKOS_INLINE_FUNCTION void init (value_type sum) const {
-            for (size_type i = 0; i < value_count; ++i) {
-                sum[i] = 0.0;
-            }
-        }
-    };
-
 }
 
 #endif
