0f32c8222c2aaf822393d7b4910a83eac352398f
Matthew Parno <mparno@solea.com>
2023-02-09 15:26:35 -0500

    Updated pip installable deployment.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     package_dir={'mpart': 'bindings/python/package'},
     package_data={'mpart':['**/*pympart*']},
     include_package_data=True,
-    cmake_args=['-DPYTHON_INSTALL_SUFFIX=bindings/python/package/', '-DMPART_JULIA:BOOL=OFF', '-DMPART_MATLAB:BOOL=OFF', '-DMPART_BUILD_TESTS:BOOL=OFF', '-DMPART_PYTHON:BOOL=ON', '-DPYTHON_INSTALL_PREFIX=']
+    cmake_args=['-DKokkos_ENABLE_THREADS:BOOL=ON', '-DPYTHON_INSTALL_SUFFIX=bindings/python/package/', '-DMPART_JULIA:BOOL=OFF', '-DMPART_MATLAB:BOOL=OFF', '-DMPART_BUILD_TESTS:BOOL=OFF', '-DMPART_PYTHON:BOOL=ON', '-DPYTHON_INSTALL_PREFIX=']
 )
