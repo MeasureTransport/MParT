c1e0f26e4373e219112fadc9d626601156359878
Daniel Sharp <dannys4@mit.edu>
2023-09-06 15:32:08 -0400

    Test TransportDistributionFactory

--- MParT/Distributions/TransportDistributionFactory.h
+++ MParT/Distributions/TransportDistributionFactory.h
@@ -3,31 +3,45 @@
 
 #include "Distribution.h"
 #include "PullbackDensity.h"
+#include "GaussianSamplerDensity.h"
 #include "PushforwardDensity.h"
 #include "PullbackSampler.h"
 #include "PushforwardSampler.h"
 
 namespace mpart {
+    namespace TransportDistributionFactory {
 
     template<typename MemorySpace>
-    using PullbackDistribution = Distribution<PullbackDensity<MemorySpace>, PullbackSampler<MemorySpace>>;
-    template<typename MemorySpace>
-    using PushforwardDistribution = Distribution<PushforwardDensity<MemorySpace>, PushforwardSampler<MemorySpace>>;
-
-    template<typename MemorySpace>
-    std::shared_ptr<PullbackDistribution<MemorySpace>> CreatePullback(std::shared_ptr<ConditionalMapBase<MemorySpace>> map, std::shared_ptr<Distribution<MemorySpace>> reference) {
-        auto sampler = std::make_shared<PullbackSampler<MemorySpace>>(map, reference->GetSampler());
-        auto density = std::make_shared<PullbackDensity<MemorySpace>>(map, reference->GetDensity());
-        return std::make_shared<PullbackDistribution<MemorySpace>>(sampler, density);
+    std::shared_ptr<Distribution<MemorySpace>> CreatePullback(std::shared_ptr<ConditionalMapBase<MemorySpace>> map, std::shared_ptr<Distribution<MemorySpace>> base) {
+        auto sampler = std::make_shared<PullbackSampler<MemorySpace>>(map, base->GetSampler());
+        auto density = std::make_shared<PullbackDensity<MemorySpace>>(map, base->GetDensity());
+        return std::make_shared<Distribution<MemorySpace>>(sampler, density);
     };
 
     template<typename MemorySpace>
-    std::shared_ptr<PushforwardDistribution<MemorySpace>> CreatePushforward(std::shared_ptr<ConditionalMapBase<MemorySpace>> map, std::shared_ptr<Distribution<MemorySpace>> reference) {
-        auto sampler = std::make_shared<PushforwardSampler<MemorySpace>>(map, reference->GetSampler());
-        auto density = std::make_shared<PushforwardDensity<MemorySpace>>(map, reference->GetDensity());
-        return std::make_shared<PushforwardDistribution<MemorySpace>>(sampler, density);
+    std::shared_ptr<Distribution<MemorySpace>> CreatePushforward(std::shared_ptr<ConditionalMapBase<MemorySpace>> map, std::shared_ptr<Distribution<MemorySpace>> base) {
+        auto sampler = std::make_shared<PushforwardSampler<MemorySpace>>(map, base->GetSampler());
+        auto density = std::make_shared<PushforwardDensity<MemorySpace>>(map, base->GetDensity());
+        return std::make_shared<Distribution<MemorySpace>>(sampler, density);
     };
 
+    template<typename MemorySpace>
+    std::shared_ptr<Distribution<MemorySpace>> CreateGaussianPullback(std::shared_ptr<ConditionalMapBase<MemorySpace>> map) {
+        unsigned int outputDim = map->outputDim;
+        auto dist = std::make_shared<GaussianSamplerDensity<MemorySpace>>(outputDim);
+        auto sampler = std::make_shared<PullbackSampler<MemorySpace>>(map, dist);
+        auto density = std::make_shared<PullbackDensity<MemorySpace>>(map, dist);
+        return std::make_shared<Distribution<MemorySpace>>(sampler, density);
+    }
+    template<typename MemorySpace>
+    std::shared_ptr<Distribution<MemorySpace>> CreateGaussianPushforward(std::shared_ptr<ConditionalMapBase<MemorySpace>> map) {
+        unsigned int inputDim = map->outputDim;
+        auto dist = std::make_shared<GaussianSamplerDensity<MemorySpace>>(outputDim);
+        auto sampler = std::make_shared<PullbackSampler<MemorySpace>>(map, dist);
+        auto density = std::make_shared<PullbackDensity<MemorySpace>>(map, dist);
+        return std::make_shared<Distribution<MemorySpace>>(sampler, density);
+    }
+    }
 };
 
 #endif // MPART_TRANSPORTDISTRIBUTIONFACTORY_H
--- tests/Distributions/Test_TransportDistributionFactory.cpp
+++ tests/Distributions/Test_TransportDistributionFactory.cpp
@@ -1,17 +1,21 @@
+#include <catch2/catch_all.hpp>
+#include "MParT/AffineMap.h"
 #include "MParT/Distributions/TransportDistributionFactory.h"
 #include "MParT/Distributions/GaussianSamplerDensity.h"
 
 
 using namespace mpart;
+using namespace mpart::TransportDistributionFactory;
 using namespace Catch;
+using MemorySpace = Kokkos::HostSpace;
 
 TEST_CASE("Creating Pullback/Pushforward distribution", "[CreatePullbackPushforward]") {
     unsigned int dim = 2;
     // Form the affine map T(x) = 2*x + [1,1]
     double diag_el = 2.0;
     double mean = 1.0;
-    Kokkos::View<double**, Kokkos::HostSpace> A("A", dim, dim);
-    Kokkos::View<double*, Kokkos::HostSpace> b("b", dim);
+    Kokkos::View<double**, MemorySpace> A("A", dim, dim);
+    Kokkos::View<double*, MemorySpace> b("b", dim);
     for(int i = 0; i < dim; i++) {
         b(i) = mean;
         for(int j = 0; j < dim; j++) {
@@ -20,10 +24,15 @@ TEST_CASE("Creating Pullback/Pushforward distribution", "[CreatePullbackPushforw
     }
 
     // Create the map and density to use
-    auto map = std::make_shared<AffineMap<Kokkos::HostSpace>>(A, b);
-    auto reference = CreateDistribution<Kokkos::HostSpace, GaussianSamplerDensity<Kokkos::HostSpace>>(dim);
+    auto map = std::make_shared<AffineMap<MemorySpace>>(A, b);
 
-    std::shared_ptr<PullbackDistribution<MemorySpace>> pullback = CreatePullback(map, reference);
-
-    std::shared_ptr<PushforwardDistribution<MemorySpace>> pushforward = CreatePushforward(map, reference);
+    SECTION("[GeneralDistributionConstruction]"){
+        auto reference = CreateDistribution<MemorySpace, GaussianSamplerDensity<MemorySpace>>(dim);
+        std::shared_ptr<Distribution<MemorySpace>> pullback = CreatePullback<MemorySpace>(map, reference);
+        std::shared_ptr<Distribution<MemorySpace>> pushforward = CreatePushforward<MemorySpace>(map, reference);
+    }
+    SECTION("Testing Gaussian shortcut") {
+        std::shared_ptr<Distribution<MemorySpace>> pullback = CreateGaussianPullback<MemorySpace>(map);
+        std::shared_ptr<Distribution<MemorySpace>> pushforward = CreateGaussianPushforward<MemorySpace>(map);
+    }
 }
